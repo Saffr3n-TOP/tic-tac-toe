9b96ff0d97f31759690b744e2b2ec477713cfb84
Saffr3n-TOP <saffr3n.reserve@gmail.com>
2023-10-23 13:48:16 +0300

    Edit script.js
    - Add new Gameboard methods: getCell, reset, getWinner and gameIsOver
    - Make variable names less verbose

--- script.js
+++ script.js
@@ -2,25 +2,73 @@
 
 const Gameboard = (() => {
   const GAMEBOARD_WIDTH = 3;
-  const GAMEBOARD_CELLS_AMOUNT = GAMEBOARD_WIDTH ** 2;
+  const WIN_CONDITIONS = [
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 4, 6],
+    [2, 5, 8],
+    [3, 4, 5],
+    [6, 7, 8]
+  ];
 
   /** @type {Array<string | undefined>} */
-  const gameboard = new Array(GAMEBOARD_CELLS_AMOUNT);
+  let gameboard = new Array(GAMEBOARD_WIDTH ** 2);
 
-  const getGameboard = () => {
+  const get = () => {
     return [...gameboard];
+  };
+
+  /** @param {number} index */
+  const getCell = (index) => {
+    return gameboard[index];
   };
 
   /**
    * @param {number} index
    * @param {string} mark
    */
-  const setGameboardCell = (index, mark) => {
+  const setCell = (index, mark) => {
     if (gameboard[index]) return;
     gameboard[index] = mark;
   };
 
-  return { getGameboard, setGameboardCell };
+  const reset = () => {
+    gameboard = new Array(GAMEBOARD_WIDTH ** 2);
+  };
+
+  const getWinner = () => {
+    for (let condition of WIN_CONDITIONS) {
+      /** @type {Array<string | undefined>} */
+      const cells = new Array(GAMEBOARD_WIDTH);
+
+      for (let i = 0; i < condition.length; i++) {
+        cells[i] = gameboard[condition[i]];
+      }
+
+      const winnerMark = cells.reduce((prevCell, currCell) => {
+        if (!currCell || currCell !== prevCell) return;
+        return currCell;
+      });
+
+      if (winnerMark) return { mark: winnerMark, condition };
+    }
+  };
+
+  const gameIsOver = () => {
+    if (!gameboard.includes(undefined) || getWinner()) return true;
+    return false;
+  };
+
+  return {
+    get,
+    getCell,
+    setCell,
+    reset,
+    getWinner,
+    gameIsOver
+  };
 })();
 
 /**
@@ -32,13 +80,14 @@ const Player = (name, mark) => {
 };
 
 ((gameboard) => {
-  /** @type {HTMLDivElement | null} */
-  const gameboardEl = document.querySelector('.gameboard');
+  const gameboardEl = /** @type {HTMLDivElement} */ (
+    document.querySelector('.gameboard')
+  );
 
   for (let i = 0; i < gameboard.length; i++) {
-    const gameboardCellEl = document.createElement('div');
-    gameboardCellEl.classList.add('cell');
-    gameboardCellEl.textContent = i % 2 === 0 ? 'O' : 'X';
-    gameboardEl?.appendChild(gameboardCellEl);
+    const cellEl = document.createElement('div');
+    cellEl.classList.add('cell');
+    cellEl.setAttribute('data-index', i.toString());
+    gameboardEl.appendChild(cellEl);
   }
-})(Gameboard.getGameboard());
+})(Gameboard.get());
